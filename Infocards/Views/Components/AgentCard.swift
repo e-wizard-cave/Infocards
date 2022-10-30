339b5df9b4a65bb401d8103161a25b95e42b4092
Eduardo Pedroza <e.wizard92@gmail.com>
2022-10-30 08:30:15 -0400

    color value corrected

--- Infocards/Views/Components/AgentCard.swift
+++ Infocards/Views/Components/AgentCard.swift
@@ -21,7 +21,7 @@ struct AgentCard: View {
                         Text(agent.name)
                             .font(.headline)
                             .foregroundColor(.red)
-                            .shadow(color: .gray, radius: 3, x: 0, y:0)
+                            .shadow(color: .black, radius: 3, x: 0, y:0)
                             .padding()
                             .background(Color(.black))
                             .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
